40945d78da9a83d599034a5ed7eb8d979c0276aa
Jorge Melnik <jmelnik19@gmail.com>
2023-10-11 18:57:31 -0300

    detalle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
-import { ContactoComponent } from './contacto/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContadorComponent,
-    ContactoComponent
   ],
   imports: [
     BrowserModule
