c7854aafefa9e893db36b74ea04ede62a71dbfd4
Jorge Melnik <jmelnik19@gmail.com>
2023-10-26 20:32:37 -0300

    async

--- src/app/contacto/components/page/page.component.ts
+++ src/app/contacto/components/page/page.component.ts
@@ -1,16 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ContactoService } from '../../services/contacto.service';
+import { Contacto } from '../../interfaces/contacto';
 
 
 @Component({
   selector: 'contacto-page',
   templateUrl: './page.component.html',
 })
-export class PageComponent {
+export class PageComponent implements OnInit {
+  public contactos = [];
 
   constructor(public contactoService: ContactoService) {
-
+    console.log("Constructor PageComponent");
   }
 
+  ngOnInit(): void {
+    this.refreshContactos();
+  }
+
+  refreshContactos (){ 
+    this.contactoService.getContactosAsync().then( data => {
+      console.log(data)
+      this.contactos = data
+    })        
+    .catch(error => {
+      console.error(error);
+        return [];
+      });
+  }
+
+  recibirContacto(contacto:Contacto){
+    this.contactoService.addContact(contacto)
+    .then(contactoCreado => {
+      console.log("Contacto creado: ", contactoCreado);
+      this.refreshContactos();
+    })
+    .catch(e => console.error("ERROR al crear contacto: ", e.message));
+  }
+
+  recibirIdParaBorrar(id:number) {
+
+  }
 
 }
--- src/app/contacto/services/contacto.service.ts
+++ src/app/contacto/services/contacto.service.ts
@@ -1,28 +1,43 @@
 import { Injectable } from '@angular/core';
+import { Observable, from } from 'rxjs';
 import { Contacto } from 'src/app/contacto/interfaces/contacto';
 
 @Injectable(
   { providedIn: 'root' }    //singleton disponible en toda la aplicación.
 )
 export class ContactoService {
+
+  private baseUrl = "http://localhost:3000/contacts";
+
   constructor() {
-    fetch("http://localhost:3000/contacts").then(res => res.json()).then(data => this.contactos = data);
+    
   }
 
-  private contactos: Contacto[] = [
-
-  ]
-  get contacts() {
-    return this.contactos;
-  }
-  recibirContacto(contacto: Contacto): void {
-    console.log("Recibí:", contacto);
-    this.contactos.push(contacto);
+  async getContactosAsync() {
+    const res = await fetch(this.baseUrl);
+    const contactos = await res.json();
+    return contactos;
   }
 
-  recibirIndiceBorrado(indice: number): void {
+  async addContact(contacto: Contacto) {
+    const res = await fetch(this.baseUrl, {
+      method: "POST",
+      body: JSON.stringify(contacto),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+
+    });
+    const data = await res.json();
+    if (res.status !== 201 ) {
+      console.error(data);
+      throw new Error(data);
+    }
+
+    return data;
+  }
+
+  removeContac(indice: number): void {
     console.log(indice);
-    this.contactos.splice(indice, 1);
+    
   }
 
 }
